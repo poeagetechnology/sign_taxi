14aad6306a90057fee145e9a731ee2d70b2ca098
srimobiles <sriammansmartstore@gmail.com>
2026-02-24 03:48:11 +0530

    updates by vignesh

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SignTaxi',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const LandingPage(),
+    );
+  }
+}
+
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
 
